5ef480f802f1dabe3f312e89ce98b7f7020da6dd
Olli Varila <olli.varila@gmail.com>
2023-09-28 03:35:50 +0300

    Added some tests for oauth router

---
+++ packages/backend/test/mocks/mockPlatform.ts
@@ -0,0 +1,30 @@
+import { BaseParams, OauthPlatform } from "../../src/api/controllers/oauth"
+import { mock, when, anything, instance } from "ts-mockito"
+import { OAuth2 } from "oauth"
+
+const oauth2Mock = mock(OAuth2)
+
+when(oauth2Mock.getAuthorizeUrl(anything())).thenReturn(
+  "http://localhost:3002/authorize"
+)
+when(
+  oauth2Mock.getOAuthAccessToken(anything(), anything(), anything())
+).thenCall((_, __, cb: Function) => {
+  cb(null, "accessToken", "refreshToken", {})
+})
+
+export const mockPlatform: OauthPlatform<BaseParams> = {
+  authorizeUrl: "http://localhost:3002/authorize",
+  redirectUrl: "http://localhost:3002/callback",
+  oauthClient: instance(oauth2Mock),
+  params: {
+    client_id: "client_id",
+    code_challenge: "code_challenge",
+    code_challenge_method: "code_challenge_method",
+    kind: "kind",
+    redirect_uri: "redirect_uri",
+    response_type: "response_type",
+    scope: "scope",
+    state: "state",
+  },
+}
---
+++ packages/backend/test/util/userFunctions.ts
@@ -0,0 +1,34 @@
+import UserModel from "../../src/api/models/UserModel"
+import jwt from "jsonwebtoken"
+import config from "../../src/config"
+import createHashedPassword from "../../src/util/createHashedPassword"
+
+namespace UserTestUtils {
+  /**
+   *
+   * @param username Default test
+   * @param password Default password
+   * @param email Default test@email.com
+   */
+  export async function createUser(
+    username: string = "test",
+    password: string = "password",
+    email: string = "test@email.com"
+  ) {
+    return UserModel.create({
+      username,
+      name: username,
+      password: createHashedPassword(password),
+      email,
+    })
+  }
+
+  /**
+   * Generate valid token for testing
+   */
+  export function genToken(userId: string) {
+    return jwt.sign({ id: userId }, config.jwt_secret)
+  }
+}
+
+export default UserTestUtils
