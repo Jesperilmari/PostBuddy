a8a319d4717561de81dae9abb96820ede4eebf60
Olli Varila <olli.varila@gmail.com>
2023-09-28 03:34:17 +0300

    Added Platform and User model/interfaces

---
+++ packages/backend/src/api/interfaces/User.ts
@@ -0,0 +1,10 @@
+import { Document } from "mongoose"
+
+export interface User extends Document {
+	username: string
+	name: string
+	email: string
+	password: string
+	// createdAt: Date;
+	// updatedAt: Date;
+}
