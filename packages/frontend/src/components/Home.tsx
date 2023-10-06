ebea897e975fbd3d8f63245fc573a9e3b2a2254a
Mikko Tanhola <mikko.tanhola@metropolia.fi>
2023-10-06 16:11:05 +0300

    frontend

--- packages/frontend/src/components/Home.tsx
+++ packages/frontend/src/components/Home.tsx
@@ -1,50 +1,61 @@
-import { useEffect, useState } from 'react'
-import MenuAppBar from './AppBar'
-import SideNav from './SideNav'
-import { PageName, pages } from '../constants'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '../reducers/store'
-import { Alert, AlertTitle } from '@mui/material'
-import useAlert from '../Hooks/useAlert'
-import { next } from '../reducers/alertReducer'
+import { useEffect, useState } from "react";
+import MenuAppBar from "./AppBar";
+import SideNav from "./SideNav";
+import { PageName, pages } from "../constants";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../reducers/store";
+import { Alert, AlertTitle } from "@mui/material";
+import useAlert from "../Hooks/useAlert";
+import { next } from "../reducers/alertReducer";
 
 function Home() {
-  const pageString = useSelector<RootState, PageName>((state) => state.page.name)
+  const pageString = useSelector<RootState, PageName>(
+    (state) => state.page.name
+  );
 
-  const page = pages.find((page) => page.name === pageString)
-  const navigate = useNavigate()
-  const token = useSelector<RootState, string | undefined>((state) => state.user.token)
-  const dispatch = useDispatch()
+  const page = pages.find((page) => page.name === pageString);
+  const navigate = useNavigate();
+  const token = useSelector<RootState, string | undefined>(
+    (state) => state.user.token
+  );
+  const dispatch = useDispatch();
 
-  const alert = useAlert()
+  const alert = useAlert();
+
   if (!token) {
-    navigate('/login')
+    navigate("/login");
   }
-
-  const [time, setTime] = useState<NodeJS.Timeout | undefined>(undefined)
+  const [time, setTime] = useState<NodeJS.Timeout | undefined>(undefined);
 
   //sets timeout for alerts
   useEffect(() => {
-    clearTimeout(time)
+    clearTimeout(time);
     if (alert.active) {
       setTime(
         setTimeout(() => {
-          dispatch(next())
-          console.log('close')
-        }, 2000),
-      )
+          dispatch(next());
+          console.log("close");
+        }, 2000)
+      );
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [alert, dispatch])
+  }, [alert, dispatch]);
 
-  const alertMessage = alert.alert.message
+  // useEffect(() => {
+  //   if (!user) {
+  //     navigate("/login");
+  //   }
+  // }, [user, navigate]);
+
+  const alertMessage = alert.alert.message;
   return (
     <>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
         }}
       >
         <Alert
@@ -52,30 +63,32 @@ function Home() {
           onClose={
             alert.alert.onClose
               ? () => {
-                  dispatch(next())
-                  console.log('close')
+                  dispatch(next());
+                  console.log("close");
                 }
               : undefined
           }
           variant={alert.alert.variant}
           sx={{
-            display: alert.active ? 'inherit' : 'none',
-            position: 'absolute',
-            minWidth: '500px',
-            top: '10%',
-            right: '5%',
+            display: alert.active ? "inherit" : "none",
+            position: "absolute",
+            minWidth: "500px",
+            top: "10%",
+            right: "5%",
             zIndex: 10,
           }}
         >
-          {alert.alert.title ? <AlertTitle>{alert.alert.title}</AlertTitle> : null}
+          {alert.alert.title ? (
+            <AlertTitle>{alert.alert.title}</AlertTitle>
+          ) : null}
           {alertMessage}
         </Alert>
         <MenuAppBar currentPage={pageString} />
         <section
           style={{
-            display: 'flex',
+            display: "flex",
             flexGrow: 1,
-            flexDirection: 'row',
+            flexDirection: "row",
           }}
         >
           <SideNav />
@@ -83,7 +96,7 @@ function Home() {
         </section>
       </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
