fcce94bfa6ec966d0c3d5db336c42138226e72b2
Olli Varila <olli.varila@gmail.com>
2023-10-10 15:07:36 +0300

    video upload implementation

--- packages/backend/src/api/controllers/uploadController.ts
+++ packages/backend/src/api/controllers/uploadController.ts
@@ -1,3 +1,4 @@
+import { PassThrough } from "stream"
 import { Request, Response } from "express"
 import { StatusCodes } from "http-status-codes"
 import { ContainerClient } from "@azure/storage-blob"
@@ -5,23 +6,19 @@ import sharp from "sharp"
 import crypto from "node:crypto"
 import APIError from "../classes/APIError"
 
-const validContentTypes = [
-  "image/jpeg", // Only images supported for now
-  "image/png",
-  // "image/gif",
-  // "video/mp4",
-  // "video/webm",
-]
+const validImageTypes = ["image/jpeg", "image/png"]
+const validVideoTypes = ["video/mp4"]
+const validContentTypes = [...validImageTypes, ...validVideoTypes]
 
 // TODO: Implement video uploads
 export default function uploadHandler(containerClient: ContainerClient) {
   return (req: Request, res: Response) => {
     // TODO might need to resize images
-    const compressor = sharp().png({ quality: 40 })
     const contentType = req.headers["content-type"]
     if (!contentType) {
       throw new APIError("Missing content-type header", StatusCodes.BAD_REQUEST)
     }
+    const compressor = getCompressor(contentType)
 
     if (!isValidContentType(contentType)) {
       res.status(400).json({
@@ -44,6 +41,25 @@ export default function uploadHandler(containerClient: ContainerClient) {
       })
     })
   }
+}
+
+function getCompressor(contentType: string): sharp.Sharp | PassThrough {
+  if (isImage(contentType)) {
+    return sharp().png({ quality: 40 })
+  }
+  if (isVideo(contentType)) {
+    return new PassThrough() // TODO implement video compression
+  }
+
+  return new PassThrough()
+}
+
+function isImage(contentType: string) {
+  return validImageTypes.includes(contentType)
+}
+
+function isVideo(contentType: string) {
+  return validVideoTypes.includes(contentType)
 }
 
 async function waitFor(ms: number) {
